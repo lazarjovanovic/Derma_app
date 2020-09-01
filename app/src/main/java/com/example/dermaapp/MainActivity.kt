da6cc6ec9be0c77ad15333c3616831b4f5ac0140
lazarjovanovic <Lufthansa4!>
2020-09-01 20:14:04 +0200

    Updated ftp to http protocol client

--- app/src/main/java/com/example/dermaapp/MainActivity.kt
+++ app/src/main/java/com/example/dermaapp/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AndroidNetworking.initialize(getApplicationContext());
+        AndroidNetworking.initialize(applicationContext);
 
         //button click
         capture_image_btn.setOnClickListener{
@@ -103,66 +103,11 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURE_MODE){
             image_view.setImageURI(image_uri)
-            //uploadtoftp(image_uri)
             uploadToHttp(image_uri)
-
-              // TEST HTTP 1
-//            val rd = RequestData("a", "e", "i")
-//            val json_data = JSONObject()
-//            json_data.put("username", rd.usr)
-//            json_data.put("password", rd.pwd)
-//            json_data.put("image", rd.img)
-//            val input: ByteArray = json_data.toString().encodeToByteArray()
-//
-//            //test 3
-//            val url = URL("http://192.168.1.5:8004/do_POST")
-//            //val url = URL("http://10.0.2.2:8004/do_POST")
-//            val con: HttpURLConnection = url.openConnection() as HttpURLConnection
-//            con.requestMethod = "POST";
-//            //con.setRequestProperty("Content-Type", "application/json; utf-8");
-//            con.setRequestProperty("Content-Type", "application/json");
-//            con.setRequestProperty("Accept", "application/json");
-//            con.doOutput = true;
-//
-//            CookieHandler.setDefault(CookieManager(null, CookiePolicy.ACCEPT_ALL))
-//
-//            val thread = Thread(Runnable {
-//                try {
-//                    val os = con.outputStream
-//                    val input_data: ByteArray = json_data.toString().encodeToByteArray()
-//                    os.write(input_data, 0, input_data.size)
-//                    os.flush()
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                }
-//            })
-//
-//            thread.start()
-
-              // TEST HTTP 2
-//            //"https://fierce-cove-29863.herokuapp.com/createAnUser"
-//            AndroidNetworking.post("http://10.0.2.2:8004")
-//                .addBodyParameter("firstname", "asd")
-//                .addBodyParameter("lastname", "das")
-//                .setTag("test")
-//                .setPriority(Priority.MEDIUM)
-//                .build()
-//                .getAsJSONObject(object : JSONObjectRequestListener {
-//                    override fun onResponse(response: JSONObject) {
-//                        // do anything with response
-//                    }
-//
-//                    override fun onError(error: ANError) {
-//                        // handle error
-//                    }
-//                })
-//            connectServer()
-
         }
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
             image_view.setImageURI(data?.data)
             image_uri = data?.data
-            //uploadtoftp(image_uri)
             uploadToHttp(image_uri)
         }
     }
@@ -186,63 +131,11 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    fun postRequest(postUrl: String, postBody: RequestBody)
-    {
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url(postUrl)
-            .post(postBody)
-            .build();
-    }
-
-    fun uploadtoftp(image_uri: Uri?){
-        val thread = Thread(Runnable {
-            try {
-                val mFtpClient = FTPClient()
-                //mFtpClient.connect("10.0.2.2", 8004)
-                //mFtpClient.connect("178.220.215.98", 8004)
-                mFtpClient.connect("192.168.1.5", 8004)
-                mFtpClient.login("derma_app_user", "derma_pass_123")
-                mFtpClient.type = FTPClient.TYPE_BINARY
-
-                //if necessary, directly mounted to the defined folder on server
-                //val currentdir = mFtpClient.currentDirectory()
-                //mFtpClient.changeDirectory("test_folder/")
-
-                //val path = getRealPathFromURI(image_uri)
-
-                val path = getPath(image_uri)
-                val path_parts = path?.split("/")
-                val img_name = path_parts?.get(path_parts.size - 1)
-                mFtpClient.upload(File(path.toString()))
-
-                val wm: WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-                val ip_addr: String = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress())
-
-                val json_data = JSONObject()
-                json_data.put("username", "test_usr")
-                json_data.put("password", "test_pwd")
-                json_data.put("image", img_name)
-                json_data.put("method", "process")
-                json_data.put("ip", ip_addr)
-
-                val myFile = File(applicationContext.filesDir, "test.txt")
-                myFile.writeText(json_data.toString())
-                mFtpClient.upload(myFile)
-
-                mFtpClient.disconnect(true)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        })
-        thread.start()
-    }
-
     fun uploadToHttp(image_uri: Uri?)
     {
         val path = getPath(image_uri)
         val file = File(path.toString())
-        var url = "http://192.168.0.12:8004/do_POST"
+        var url = "http://192.168.1.5:8004/do_POST"
 
         val req = RequestBody.create(MediaType.parse("image/png"), file)
 
@@ -264,28 +157,6 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun getRealPathFromURI(uri: Uri?): String? {
-        var path = ""
-        if (contentResolver != null) {
-            val cursor: Cursor? = contentResolver.query(uri!!, null, null, null, null)
-            if (cursor != null) {
-                cursor.moveToFirst()
-                val idx: Int = cursor.getColumnIndex(Images.ImageColumns.DATA)
-                path = cursor.getString(idx)
-                cursor.close()
-            }
-        }
-        return path
-    }
-
-    fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
-        val bytes = ByteArrayOutputStream()
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val path =
-            Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
-        return Uri.parse(path)
-    }
-
     fun getPath(uri: Uri?): String? {
         val projection =
             arrayOf(Images.Media.DATA)
@@ -297,16 +168,5 @@ class MainActivity : AppCompatActivity() {
         val s = cursor.getString(column_index)
         cursor.close()
         return s
-    }
-
-    fun connectServer() {
-        val ipv4Address = "10.0.2.2"
-        val portNumber = "8004"
-        val postUrl = "http://$ipv4Address:$portNumber/"
-        val postBodyText = "Hello"
-        val mediaType: MediaType = MediaType.parse("text/plain; charset=utf-8")
-        val postBody: RequestBody = RequestBody.create(mediaType, postBodyText)
-
-        postRequest(postUrl, postBody)
     }
 }
